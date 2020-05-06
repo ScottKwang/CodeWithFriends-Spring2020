c06f800ed32fbddcc4875b8a10ee2533c67ddb0f
KevinBNaughton <kevinbnaughton@gmail.com>
2020-05-06 17:37:52 -0400

    Started on MIDI Sequence

--- projects/musiotic/src/MusicComponents/MIDISequence.java
+++ projects/musiotic/src/MusicComponents/MIDISequence.java
@@ -1,4 +1,21 @@
 package MusicComponents;
 
 public abstract class MIDISequence {
+
+    // Refer to Camelot wheel in /assets/camelot.png.
+    // Default: 8B (C Major).
+    String key = "8B";
+
+    // 4 / 4 Time. The top number signifies 4 notes of the bottom number type of note.
+    // Default: 4 quarter notes per measure.
+    int timeSigTop = 4;
+    int timeSigBot = 4;
+
+    // How many measures the MIDI Sequence will have.
+    // Default: 4 measures (4 bar loops).
+    int measures = 4;
+
+    // Todo: Java Sequencers?
+
+
 }
