8526d8110cc1bdbe90f52e6a4a117e721a6d928d
Brian Nieves <brian@bitasy.me>
2020-05-08 16:59:12 -0400

    add javafx

--- projects/musiotic/src/MusioticLauncher.java
+++ projects/musiotic/src/MusioticLauncher.java
@@ -1,4 +1,21 @@
-public class MusioticLauncher {
+import javafx.stage.Stage;
+import jm.music.data.*;
+import jm.util.*;
+
+import static jm.constants.Durations.MINIM;
+import static jm.constants.Pitches.C4;
+
+import javafx.application.Application;
+
+public class MusioticLauncher extends Application{
     public static void main(String[] args){
+        Score s = new Score(new Part(new Phrase(new Note(C4, MINIM))));
+        Play.midi(s);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("meow");
     }
 }
