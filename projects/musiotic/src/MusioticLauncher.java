355025d25a70df8fb32e078ec29a10cc386d7a52
Brian Nieves <brian@bitasy.me>
2020-05-09 19:54:29 -0400

    create more developed start screen

--- projects/musiotic/src/MusioticLauncher.java
+++ projects/musiotic/src/MusioticLauncher.java
@@ -1,4 +1,4 @@
-import UserInterface.MainScreen;
+import UserInterface.StartScreen;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,20 +9,29 @@ import jm.music.data.Score;
 import jm.util.Play;
 
 import static jm.constants.Durations.EIGHTH_NOTE;
+import static jm.constants.Durations.SIXTEENTH_NOTE;
 import static jm.constants.Pitches.C4;
+import static jm.constants.Pitches.C5;
+import static jm.constants.Pitches.E4;
+import static jm.constants.Pitches.G4;
 
 public class MusioticLauncher extends Application{
     public static void main(String[] args){
-        Score s = new Score(new Part(new Phrase(new Note(C4, EIGHTH_NOTE))));
-        Play.midi(s);
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene mainScene = new Scene(new MainScreen(), 1280, 720);
+        Scene mainScene = new Scene(new StartScreen(), 1280, 720);
         stage.setScene(mainScene);
         stage.setTitle("Musiotic");
         stage.show();
+        Phrase intro = new Phrase();
+        intro.add(new Note(C4, SIXTEENTH_NOTE));
+        intro.add(new Note(E4, SIXTEENTH_NOTE));
+        intro.add(new Note(G4, SIXTEENTH_NOTE));
+        intro.add(new Note(C5, EIGHTH_NOTE));
+        Score s = new Score(new Part(intro));
+        Play.midi(s);
     }
 }
---
+++ projects/musiotic/src/UserInterface/StartScreen.java
@@ -0,0 +1,54 @@
+package UserInterface;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
+import java.io.File;
+
+public class StartScreen extends BorderPane {
+    public StartScreen(){
+        setBackground(new Background(new BackgroundFill(Color.BLUEVIOLET, null, null)));
+        var newSongBtn = new Button("Create New Song");
+        var currSongsBtn = new Button("View Existing Songs");
+        var buttons = new VBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setSpacing(100);
+        buttons.setPrefWidth(800);
+        buttons.setMaxWidth(800);
+        buttons.setMaxHeight(Double.MAX_VALUE);
+        buttons.setPadding(new Insets(-200, 150, 0, 150));
+        newSongBtn.setMaxWidth(Double.MAX_VALUE);
+        currSongsBtn.setMaxWidth(Double.MAX_VALUE);
+        buttons.getChildren().addAll(newSongBtn, currSongsBtn);
+
+        var center = new HBox();
+        center.setPadding(new Insets(20, 0, 0, 100));
+        var image = new ImageView();
+        image.setFitWidth(400);
+        image.setPreserveRatio(true);
+        File musicNote = new File("projects/musiotic/assets/images/music_note.png");
+        image.setImage(new Image(musicNote.toURI().toString()));
+
+        center.getChildren().addAll(image, buttons);
+
+        var name = new Label("Musiotic");
+        name.setMaxWidth(Double.MAX_VALUE);
+        name.setAlignment(Pos.CENTER);
+        name.setFont(Font.font("sans-serif", 90));
+        name.setTextFill(Paint.valueOf("black"));
+        setTop(name);
+        setCenter(center);
+    }
+}
