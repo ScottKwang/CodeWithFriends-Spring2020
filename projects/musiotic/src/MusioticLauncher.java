b3e41f322f6cdb972e48e01db82bf9ee734575c7
Brian Nieves <brian@bitasy.me>
2020-05-08 20:46:37 -0400

    Begin creating main screen UI

--- projects/musiotic/src/MusioticLauncher.java
+++ projects/musiotic/src/MusioticLauncher.java
@@ -1,21 +1,28 @@
-import javafx.stage.Stage;
-import jm.music.data.*;
-import jm.util.*;
-
-import static jm.constants.Durations.MINIM;
-import static jm.constants.Pitches.C4;
-
+import UserInterface.MainScreen;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jm.music.data.Note;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+import jm.util.Play;
+
+import static jm.constants.Durations.EIGHTH_NOTE;
+import static jm.constants.Pitches.C4;
 
 public class MusioticLauncher extends Application{
     public static void main(String[] args){
-        Score s = new Score(new Part(new Phrase(new Note(C4, MINIM))));
+        Score s = new Score(new Part(new Phrase(new Note(C4, EIGHTH_NOTE))));
         Play.midi(s);
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("meow");
+        Scene mainScene = new Scene(new MainScreen(), 1280, 720);
+        stage.setScene(mainScene);
+        stage.setTitle("Musiotic");
+        stage.show();
     }
 }
--- projects/musiotic/src/UserInterface/MainScreen.java
+++ projects/musiotic/src/UserInterface/MainScreen.java
@@ -1,4 +1,23 @@
 package UserInterface;
 
-public class MainScreen {
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class MainScreen extends BorderPane {
+    public MainScreen(){
+        var newSongBtn = new Button("Create New Song");
+        var currSongsBtn = new Button("View ExistingSongs");
+        var buttons = new VBox(200, newSongBtn, currSongsBtn);
+        var center = new AnchorPane();
+        center.getChildren().add(buttons);
+        AnchorPane.setRightAnchor(buttons, 30.0);
+        var name = new Label("Musiotic");
+        name.setPadding(new Insets(10, 10, 10, 10));
+        setTop(name);
+        setCenter(center);
+    }
 }
