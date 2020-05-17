83065f31053a440694e54beb5a7bfa64814a4e4f
Brian Nieves <brian@bitasy.me>
2020-05-16 23:17:08 -0400

    add key change methods to SongManager

--- projects/musiotic/src/app/song/SongManager.java
+++ projects/musiotic/src/app/song/SongManager.java
@@ -1,5 +1,13 @@
 package song;
 
+import jm.JMC;
+import jm.music.data.Note;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+import jm.music.tools.Mod;
+
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -8,11 +16,17 @@ public class SongManager {
     public final StylePhase stylePhase;
     public Map<Phase.Type, Phase> phaseMap;
     protected Phase currentPhase;
+    private Score score;
+    private int tonic;
+    private int[] mode;
 
 
     public SongManager(){
         stylePhase = new StylePhase(this);
         currentPhase = stylePhase;
+        score = new Score();
+        tonic = JMC.C4;
+        mode = JMC.MAJOR_SCALE;
     }
 
     private Consumer<Collection<Phase>> onStyleSelect;
@@ -30,5 +44,32 @@ public class SongManager {
 
     public void goToPhase(Phase.Type type){
 
+    }
+
+    public void changeTonic(int tonic){
+        Mod.transpose(score, tonic - this.tonic);
+        this.tonic = tonic;
+    }
+
+    public void changeMode(int[] mode) {
+        if (mode.length != this.mode.length) {
+            throw new IllegalArgumentException("New mode must be the same size as previous mode.");
+        }
+        int[] diff = new int[mode.length];
+        for (int i = 0; i < mode.length; i++) diff[i] = mode[i] - this.mode[i];
+
+        for (Part part : score.getPartArray())
+            for (Phrase phrase : part.getPhraseArray())
+                for (Note note : phrase.getNoteArray()) {
+                    if (note.getPitchType() == Note.MIDI_PITCH) {
+                        var pitch = note.getPitch();
+                        var inScale = (pitch + tonic) % 12;
+                        var ordInScale = Arrays.binarySearch(this.mode, inScale);
+                        if (ordInScale >= 0)
+                            Mod.transpose(note, diff[ordInScale]);
+                    }
+                }
+
+        this.mode = mode;
     }
 }
