c6bf01965ed6e1e8d6b85c22c865df646279b91c
sttic <git@tommydeng.com>
2020-05-28 22:59:20 -0400

    clean up default Next app

---
+++ projects/sttic/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div className="container">
+			<Head>
+				<title>Digital Window</title>
+			</Head>
+
+			<main></main>
+		</div>
+	);
+}
