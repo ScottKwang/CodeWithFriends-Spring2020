e8c77695ca408a52740316dcce92a0c17d27a9ee
lamirb591 <34675714+lamirb591@users.noreply.github.com>
2020-05-30 22:04:20 +0100

    Update setup.sql

--- projects/lamirb591/MyDiary/files/setup.sql
+++ projects/lamirb591/MyDiary/files/setup.sql
@@ -1,21 +1,20 @@
 create database codewithfriends;
 
 create table users(
-	user_login varchar(255)primary_key not null,
+	user_login varchar(255)primary key not null,
 	user_name varchar(255) not null,
 	user_last_name varchar(255) not null,
-	user_password varchar(255),
+	user_password varchar(255) not null,
 	user_pic blob );
 	
 create table posts(
-	post_id int auto_increment not null primary_key,
+	post_id int auto_increment not null primary key,
 	user_login varchar(255) not null,
-	FOREIGN KEY (user_login) REFERENCES users(user_login),
 	post_date not null,
 	post_title varchar(255) not null,
-	post_content varchar(255) not null,
+	post_content longtext not null,
 	post_rate enum('1','2','3') not null,
 	post_visibility enum('0','1') not null,
-	post_pic blob
-
+	post_pic blob,
+        FOREIGN KEY (user_login) REFERENCES users(user_login)
 );
