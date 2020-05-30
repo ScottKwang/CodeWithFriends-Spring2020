cd927932187dbc75b43964831cda694645f7870c
FadhillahCakti <36729011+FadhillahCakti@users.noreply.github.com>
2020-05-30 10:45:06 +0700

    Upload to /Serba-Serbi 48 ext 2

---
+++ projects/Serba-Serbi 48/app/Info48.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Info48 extends Model
+{
+    protected $table = 'info48s';
+    protected $fillable = ['gambar','judul','rintisan','tag_namagrup','selengkapnya'];
+}
